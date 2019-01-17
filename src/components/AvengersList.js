1c8eb4fa442b039c95baaaaeed719eca660bbe98
ltann <leeroyleeroy18@gmail.com>
2019-01-16 19:59:55 -0800

    worked with history object

--- src/components/AvengersList.js
+++ src/components/AvengersList.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+
 
 function AvengersList(props) {
     return (
@@ -7,12 +8,19 @@ function AvengersList(props) {
             {props.avengers.map(avenger => (
                 <div className="character-card" key={avenger.id} >
                     <img src={avenger.thumbnail} alt={avenger.name} />
-                    <h2><Link to={`/avengers/${avenger.id}`}>{avenger.name}</Link></h2>
+                    <h2>
+                        {/* <Link to={`/avengers/${avenger.id}`}>{avenger.name}</Link> */}
+                        <div onClick={() => routetoAvengers(props, avenger)} >{avenger.name}</div>
+                    </h2>
                     <p>({avenger.nickname})</p>
                 </div>
             ))}
         </div>
     )
+}
+
+function routetoAvengers(props, avenger) {
+    props.history.push(`/avengers/${avenger.id}`)
 }
 
 export default AvengersList;
